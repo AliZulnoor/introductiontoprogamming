ee35c5f25bda5665209c2c6f51fbe5d66c8dbc44
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2021-12-19 00:11:02 -0800

    Update and rename readme.txt to datatypes.cpp

---
+++ Lecture 4/datatypes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int firstNumber = 1;
+
+    cout << "firstNumber = " << firstNumber << endl;
+
+    bool iCanCode = true;
+
+    cout << "iCanCode = " << iCanCode << endl;
+
+    char hopefulGrade = 'a';
+
+    cout << "hopefulGrade = " << hopefulGrade << endl;
+
+    float myDecimal = 1.0;
+
+    cout << "myDecimal = " << myDecimal << endl;
+
+    string minimalSentance = "y";
+
+    cout << "minimalSentance = " << minimalSentance << endl;
+
+    long keyMash = 13213123;
+
+    cout << "keyMash = " << keyMash << endl;
+
+    float mysteryDataType = 5.6; //Use Google to work this out
+
+    cout << "mysteryDataType = " << mysteryDataType << endl;
+
+    return 0;
+}
