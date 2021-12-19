84536d65f38e111541d2f9c2c040f7e0e298f06d
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2021-12-19 00:20:06 -0800

    Create Temperature2.cpp

---
+++ Lecture 5/Temperature2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float fahrenheit; //Initializing Fahrenheit as float 
+
+	float celcius; //Initializing Celsius as float
+
+	cout << "Enter the temperature in fahrenheit: " ;
+
+	cin >> fahrenheit; // Taking the temperature in Fahrenheit as input from the user
+
+	cout << fahrenheit << " fahrenheit is " << (fahrenheit - 32) * 5 / 9 << " in Celsius." << endl; // Formula = (F-32)*5/9 to change the temperature into celcius
+
+	return 0;
+}
