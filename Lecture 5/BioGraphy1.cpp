5f77d1013efb9f1b39df17b6a81e7c5fccaac543
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2021-12-19 00:17:26 -0800

    Create BioGraphy1.cpp

---
+++ Lecture 5/BioGraphy1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name = "Ali"; // Using string as datatype
+	
+	int age = 18; // Using integer as Data type
+
+	string hometown = "lahore"; // Using String as Data type
+
+	cout << "Name: " << name << endl << "Age:  " << age << endl << "Hometown: " << hometown << endl; // Printing all information using cout once
+
+	return 0;
+}
