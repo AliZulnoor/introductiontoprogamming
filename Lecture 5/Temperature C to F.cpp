3fd39d9789a7d40cdaf2a23298dfb9bacf167502
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2021-12-19 00:30:44 -0800

    Add files via upload

---
+++ Lecture 5/Temperature C to F.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float c = 34;
+	float f;
+	cout << "Fahrenhit is : " << (c*9 / 5)  + 32 << endl; // (c*9/5)+ 32 is the formula to convert celcius into fahrenhit
+	return 0;
+}
