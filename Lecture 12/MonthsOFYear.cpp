b81a1b9268039481ecc9f810f723c1e637ad0781
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2021-12-20 01:42:38 -0800

    Create MonthsOFYear.cpp

---
+++ Lecture 12/MonthsOFYear.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string i[] = {"january","febraury","march","april","may", "june", "july", "august", "september", "october", "november", "december"};
+	return 0;
+}
