89cdebb8f0da8d095b8d1085b92d7cac4dc7b4b9
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2021-12-20 03:26:02 -0800

    Create PointlessBox.cpp

---
+++ Lecture 9/PointlessBox.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter a number either 1 or 2\n";
+    int x; 
+    cin >> x;
+    while (x != 0 && x <= 2 && !cin.fail())
+    {
+        if (x == 1)
+        {
+            cout << "you have entered the number 1\n";
+        }
+        else
+        {
+            cout << "you have entered the number 2\n";
+        }
+
+        cout << "Enter a number either 1 or 2\n";
+        cin >> x;
+     
+    }
+cout << "You did not enter the number 1 or 2";
+return 0;
+}
