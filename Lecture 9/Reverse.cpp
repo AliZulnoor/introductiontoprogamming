9d4ff0c5528f989659dd2a6a3974c681a0ad9afd
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2021-12-20 03:24:03 -0800

    Create Reverse.cpp

---
+++ Lecture 9/Reverse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>  
+using namespace std;
+
+int main() 
+{
+
+    int num=108;
+    
+    while (num>=9 )  //while conditional check
+    {
+        cout << num << endl;
+        num = num - 9;
+              
+    }
+
+    cin.get(); //keeps console window open in Visual  Studio
+    return 0;
+}
