13dc2e57cb05c055354233b6ff1a8d310d836db3
AliZulnoor <90544813+AliZulnoor@users.noreply.github.com>
2021-12-20 01:42:16 -0800

    Delete MonthsOFYear.cpp

--- Lecture 11/MonthsOFYear.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	string i[] = {"january","febraury","march","april","may", "june", "july", "august", "september", "october", "november", "december"};
-	return 0;
-}
